470c6edf0a045da405fffeecaf8720cb904bd3c5
pyong-1459 <58324470+pyong-1459@users.noreply.github.com>
2019-11-29 17:00:50 +0900

    Update gf_mul.m

--- gf_mul.m
+++ gf_mul.m
@@ -1,21 +1,5 @@
 function out = gf_mul(a, b, poly, m)
 
-% agen = fliplr(gfprimdf(m,2)); 
-% alpha = zeros(n,m);
-% alpha(1,:) = [zeros(1,m-1), 1];                                         % alpha(1) 은 알파 1승이 되어야 하므로 0010, 0001부터 하면 알파 0승임
-% %--------------------------------------------------------------------------
-% for i = 2:n                                                              % 알파 1승부터 14승까지 polynomial notation 구함
-%     if(alpha(i-1,1) == 1)
-%         alpha(i, :) = xor([alpha(i - 1, 2:m) 0], agen(1, 2:m + 1));
-%     else
-%         alpha(i, :) = [alpha(i - 1, 2:m) 0];
-%     end
-% end
-
-% m = 4;
-% poly = [1 0 0 1 1];
-% a = [1 0 1 0];
-% b = [0 1 0 1];
 if a == zeros(1,m)
     out = zeros(1,m);
 elseif b == zeros(1,m)
